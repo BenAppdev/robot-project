d9296edfe0cbdd29f117f4fe8699de3544de5164
BenAppdev <benjaminangulo@gmail.com>
2025-10-04 00:34:33 -0400

    Update run_test.sh for passwordless SSH

--- run_test.sh
+++ run_test.sh
@@ -7,5 +7,5 @@ REPO_PATH="/home/pi/robot-project"
 python3 server.py &
 SERVER_PID=$!
 sleep 2
-ssh $PI_SSH_USER@$PI_SSH_HOST "cd $REPO_PATH && python3 client.py"
+ssh $PI_SSH_USER@$PI_SSH_HOST "cd $REPO_PATH && source venv/bin/activate && python3 client.py"
 kill $SERVER_PID 2>/dev/null || true
