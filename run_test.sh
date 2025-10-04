8d70e1f63f4e3605ef7ad616f072d78beb881f09
BenAppdev <benjaminangulo@gmail.com>
2025-10-04 00:48:04 -0400

    Revert run_test.sh to use passwordless SSH

--- run_test.sh
+++ run_test.sh
@@ -3,10 +3,9 @@
 PI_SSH_USER="pi"
 PI_SSH_HOST="192.168.0.86"
 REPO_PATH="/home/pi/robot-project"
-PI_PASSWORD="102355"  # Replace with Pi's actual password
 
 python3 server.py &
 SERVER_PID=$!
 sleep 2
-sshpass -p "$PI_PASSWORD" ssh $PI_SSH_USER@$PI_SSH_HOST "cd $REPO_PATH && source venv/bin/activate && python3 client.py"
+ssh $PI_SSH_USER@$PI_SSH_HOST "cd $REPO_PATH && source venv/bin/activate && python3 client.py"
 kill $SERVER_PID 2>/dev/null || true
